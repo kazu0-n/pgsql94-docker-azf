3312ad74e46210db6dca9343333dbcd19eba5ed4
kazu0-n <k-namba@rhizome-e.com>
2016-02-21 22:24:10 +0900

    now i creating create_db.sh

---
+++ create_db.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+psql -U postgres --command "CREATE USER ${DB_USER} WITH CREATEDB PASSWORD '${DB_PASS}';"
+psql -U postgres --command "CREATE DATABASE ${DB_NAME} OWNER ${DB_USER} TEMPLATE template0;"
