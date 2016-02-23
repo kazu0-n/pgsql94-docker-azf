a60cbf11ff582c77cd4aa3db3a2dbba67452b1f0
kazu0-n <k-namba@rhizome-e.com>
2016-02-23 10:11:32 +0900

    Creating PITR WAL Archive directory.

--- create_db.sh
+++ create_db.sh
@@ -2,3 +2,5 @@
 
 psql -U postgres --command "CREATE USER ${DB_USER} WITH CREATEDB PASSWORD '${DB_PASS}';"
 psql -U postgres --command "CREATE DATABASE ${DB_NAME} OWNER ${DB_USER} TEMPLATE template0;"
+
+mkdir /mnt/pgsql/archive
